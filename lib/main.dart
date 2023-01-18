e49c44fe965c3bf420df665a29176cc24fc76160
jeminblack <jeminthesiya101@gmail.com>
2023-01-18 17:57:57 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:age/cal.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Cal(),
+    ),
+  );
+}
